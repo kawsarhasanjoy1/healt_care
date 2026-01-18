6a29cecfcd2df610eaf6e83d21c01c0eb663809b
kawsarhasanjoy1 <kawsarhasanjoy1234@gmail.com>
2026-01-17 21:33:50 -1200

    update

--- dist/app.js
+++ dist/app.js
@@ -9,7 +9,7 @@ import globalErrorHandler from "./app/middleware/GlobalErrorHandler.js";
 export const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "https://healt-care-frtd.vercel.app", credentials: true }));
 app.use(cookieParser());
 app.get("/", (req, res) => {
     res.json({
